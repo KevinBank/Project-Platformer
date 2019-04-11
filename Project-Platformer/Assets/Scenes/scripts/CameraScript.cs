b6762b573bc953065f1bca1efe65c9db28e5bfa0
kevin <kevinb2002@live.nl>
2019-04-11 14:10:16 +0200

    redone scene

---
+++ Project-Platformer/Assets/Scenes/scripts/CameraScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private float currentMouseH;
+    private float currentMouseV;
+
+    private float previousMouseH;
+    private float previousMouseV;
+
+    private Vector3 mousePos;
+    //public Vector3 cameraAngle;
+
+    private bool inGame = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //cameraAngle = 
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            inGame = !inGame;
+            Debug.Log(inGame);
+        }
+
+        mousePos = Input.mousePosition;
+        mousePos = mousePos / 10;
+        Debug.Log(mousePos);
+        //transform.Rotate(Vector3.left * mousePos.y / 50);
+        //transform.Rotate(Vector3.up * mousePos.x / 50);
+
+        //transform.localRotation.SetEulerRotation(currentMouseH - previousMouseH,currentMouseV - previousMouseV,0);
+        transform.localRotation = Quaternion.Euler(new Vector3(-mousePos.y, mousePos.x, 0));
+    }
+
+
+}
---
+++ Project-Platformer/Assets/Scenes/scripts/PlayerScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed;
+    public float fUp = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+            //rb.AddRelativeForce(rb.velocity.x, rb.velocity.y, speed);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+            //rb.AddRelativeForce(rb.velocity.x, rb.velocity.y, -speed);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+            //rb.AddRelativeForce(-speed, rb.velocity.y, rb.velocity.z);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+            //rb.AddRelativeForce(speed, rb.velocity.y, rb.velocity.z);
+        }
+
+        if (Input.GetKey("space") && onGround)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, fUp, rb.velocity.z);
+        }
+    }
+
+    private bool onGround = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground") onGround = true;
+
+        Debug.Log("on ground" + onGround);
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground") onGround = false;
+    }
+
+}
