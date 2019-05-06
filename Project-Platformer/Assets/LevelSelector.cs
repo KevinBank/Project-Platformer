0ee10bed328befc467c9671c5549f175789da85d
kevin <kevinb2002@live.nl>
2019-05-06 19:30:32 +0200

    1.08

---
+++ Project-Platformer/Assets/LevelSelector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+
+    public void loadLevel01()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+    public void loadHelp()
+    {
+        SceneManager.LoadScene("Help");
+    }
+}
